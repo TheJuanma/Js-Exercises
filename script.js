f536eb0666ee0a62195b368043953d1c2213f68f
Thejuanma <jumanuelro.14@gmail.com>
2023-05-25 15:36:59 -0500

    Primero: se hacen ejercicios de 3 al 6

---
+++ script.js
@@ -0,0 +1,57 @@
+
+/*Tercer ejercicio
+3. Crear un programa que determine si un número introducido en un Prompt
+es par o no, la respuesta será mostrada en un Alert.
+
+let requestedNumber = parseInt(prompt(`Introduce un numero`));
+
+if ( (requestedNumber % 2) === 0){
+    alert(`El numero que ingreso es par`);
+} else if ( requestedNumber % 2 === 1){
+    alert(`El numero que ingreso no es par`);
+}
+*/
+
+/*Cuarto ejercicio
+4. Solicitar al usuario un número de cliente. Si el número es el 1000, imprimir
+"Ganaste un premio", en caso contrario mostrar el número y el mensaje
+“Lo sentimos, sigue participando”.
+
+let number = parseInt(prompt(`Ingrese un numero`));
+
+if ( number === 1000 ){
+    console.log(`Ganaste un premio`);
+} else {
+    console.log(`Lo sentimos, sigue participando`);
+}
+*/
+
+/*Quinto ejercicio
+5. Solicitar al usuario que ingrese dos números y mostrar cuál de los dos es
+menor. No considerar el caso en que ambos números son iguales.
+
+let firtsNumber = parseInt(prompt(`Ingresa un numero`));
+let secondNumber = parseInt(prompt(`Ingresa otro numero`));
+
+if ( firtsNumber > secondNumber ){
+    console.log(`El primer numero es mayor al segundo numero`);
+} else if ( firtsNumber < secondNumber ){
+    console.log(`El segundo numero es mayor al primer numero`);
+}
+*/
+
+/*Sexto ejercicio
+6. Solicitar al usuario que ingrese tres números y mostrar cuál de los tres es
+el número mayor. Considerar el caso en que 2 números sean iguales.*/
+
+let firtsNumber = parseInt(prompt(`Ingresa un numero`));
+let secondNumber = parseInt(prompt(`Ingresa otro numero`));
+let thirdNumber = parseInt(prompt(`Ingresa otro numero`));
+
+if (){
+    console.log (`El primer numero es mayor`);
+} else if (){
+    console.log (`El segundo numero es mayor`);
+} else if (){
+    console.log (`El segundo numero es mayor`);
+}
